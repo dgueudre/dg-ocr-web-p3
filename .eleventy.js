0aaf9c4780c36fa1c1f03d8276ceb2c089636e09
dgueudre <dgueudre@gmail.com>
2022-08-02 22:09:01 +0200

    site

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = config => {
+  config.addPassthroughCopy('./assets/');
+
+  config.addNunjucksFilter("bytype", (dishes, type) => dishes.filter(dish => dish.type === type));
+
+  return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: 'docs'
+    }
+  };
+};
